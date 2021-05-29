268d939580a61364ffbc91474b50a2edf5cb249c
cary <caryz@terpmail.umd.edu>
2021-05-29 11:57:08 -0400

    [CBW-2] Added basic functionality for button and randomized text display

--- CowboyWisom/ViewController.swift
+++ CowboyWisom/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wrapperView: UIView!
+    @IBOutlet weak var wisdomLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        wisdomLabel.text = "Tap the button to get some wisdom!"
+        wrapperView.layer.borderWidth = 1
+        wrapperView.layer.borderColor = UIColor.lightGray.cgColor
+        wrapperView.layer.cornerRadius = 8
     }
 
+    @IBAction func wisdomButtonTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5) { [weak self] in
+            self?.wrapperView.alpha = 0
+            self?.wisdomLabel.text = WisdomData.getRandomQuote()
+            self?.wrapperView.alpha = 1
+        }
+    }
 
 }
 
